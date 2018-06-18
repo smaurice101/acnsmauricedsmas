4a1a980d048d5b1b5e5266f2fc31a6ca6a6f86c8
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2018-06-18 15:52:56 -0400

    added to init

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .sendfiles import returndata
+from .sendfiles import uploadcsvfortraining
+from .sendfiles import getpredictions
+
+from .callmas import dotraining
+from .callmas import dopredictions
+
+
+name = "acnsmauricedsmas"
